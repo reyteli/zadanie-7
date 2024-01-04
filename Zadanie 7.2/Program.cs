b0f4c400bf33bf4c8ba851255d10f293b98f55cc
Никита Олегович Романенко <nikcom526@mail.ru>
2024-01-04 11:39:08 +0300

    Добавьте файлы проекта.

---
+++ Zadanie 7.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Zadanie_7._2
+{
+    class Program
+    {   
+            static void Main(string[] args)
+            { 
+                Console.WriteLine("Введите длинну ребра куба:");
+                int a = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Обьем куба:" + Math.Pow(a, 3) + " куб.см");
+                Console.WriteLine("Площадь поверхности:" + Math.Pow(a, 2) * 6 + " кв.см");
+                Console.ReadLine();
+            }
+        
+    }
+}
---
+++ zadanie 7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace zadanie_7
+{
+    class Program
+    {
+        public struct Triangle
+        {
+            public double x, y, z;
+            public double Square()
+            {
+                double p = (x + y + z) / 2;
+                double s = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+                return s;
+            }
+        }
+        static void Main()
+        {
+            Triangle[] triagles = new Triangle[2];
+            for (int i = 0; i < triagles.Length; i++)
+            {
+                Console.WriteLine("Введите длины (x,y,z) сторон треугольника {0}", i + 1);
+                triagles[i].x = double.Parse(Console.ReadLine());
+                triagles[i].y = double.Parse(Console.ReadLine());
+                triagles[i].z = double.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < triagles.Length; i++)
+            {
+                Console.WriteLine("Площадь {0}-го треугольника = {1}", i + 1, triagles[i].Square());
+            }
+            int maxSquaredTriIdx = 0;
+            for (int i = 1; i < triagles.Length; i++)
+            {
+                if (triagles[i].Square() > triagles[maxSquaredTriIdx].Square())
+                    maxSquaredTriIdx = i;
+            }
+            Console.WriteLine("Макс площадь у {0}-го треугольника", maxSquaredTriIdx + 1);
+
+            Console.ReadKey();
+        }
+        }
+}
